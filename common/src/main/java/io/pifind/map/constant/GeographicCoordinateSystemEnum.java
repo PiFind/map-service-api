b0c0f3df105bc78a035f165194c2fcbe6c70ac82
Zhang Ming <zmgithub@163.com>
2023-01-13 17:29:29 +0800

    更新坐标枚举

--- common/src/main/java/io/pifind/map/constant/GeographicCoordinateSystemEnum.java
+++ common/src/main/java/io/pifind/map/constant/GeographicCoordinateSystemEnum.java
@@ -16,12 +16,24 @@ public enum GeographicCoordinateSystemEnum {
     /**
      * 由中国国家测绘局制定的地理坐标系统。
      */
-    GCJ0,
+    GCJ02,
 
     /**
      * 中国北斗系统所使用的坐标系。
      */
     CGCS2000,
     ;
+
+    private static Map<String,GeographicCoordinateSystemEnum> map;
+
+    public static GeographicCoordinateSystemEnum parse(String str) {
+        if (map == null) {
+            map = new HashMap<>();
+            for (GeographicCoordinateSystemEnum systemEnum : values()) {
+                map.put(systemEnum.name().toLowerCase(),systemEnum);
+            }
+        }
+        return map.get(str.toLowerCase());
+    }
 
 }
